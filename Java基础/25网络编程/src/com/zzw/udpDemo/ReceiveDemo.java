0339119a4ac4b0d25ef5cdd7ccae4f5bc72a7226
xxBei <zzw_bei@163.com>
2018-09-22 22:38:49 +0800

    UDP接收端

---
+++ Java基础/25网络编程/src/com/zzw/udpDemo/ReceiveDemo.java
@@ -0,0 +1,15 @@
+package com.zzw.udpDemo;
+
+/**
+ * UDP协议接收端
+ *      创建接收端Socket对象
+ *      接收数据
+ *      解析数据
+ *      输出数据
+ *      释放资源
+ * */
+public class ReceiveDemo {
+    public static void main(String[] args) {
+
+    }
+}

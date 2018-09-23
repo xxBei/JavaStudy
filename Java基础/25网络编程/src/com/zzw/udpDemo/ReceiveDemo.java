94d25f659dc40c52b0769c45a0a163587dbec655
xxBei <zzw_bei@163.com>
2018-09-23 21:25:39 +0800

    接收UDP数据

--- Java基础/25网络编程/src/com/zzw/udpDemo/ReceiveDemo.java
+++ Java基础/25网络编程/src/com/zzw/udpDemo/ReceiveDemo.java
@@ -1,5 +1,11 @@
 package com.zzw.udpDemo;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
 /**
  * UDP协议接收端
  *      创建接收端Socket对象
@@ -9,7 +15,27 @@ package com.zzw.udpDemo;
  *      释放资源
  * */
 public class ReceiveDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //创建接收端Socket对象
+        DatagramSocket ds = new DatagramSocket(8888);
 
+        //接收数据
+        byte[] bytes = new byte[1024];
+        DatagramPacket dp = new DatagramPacket(bytes,bytes.length);
+
+        System.out.println("等待接收数据");
+        ds.receive(dp);//阻塞
+        System.out.println("接收数据开始");
+
+        //解析数据
+        InetAddress address = dp.getAddress();//获取ip地址
+        byte[] data = dp.getData();//获取数据
+        int length = dp.getLength();//获取数据长度
+
+        //输出数据
+        System.out.println("sendUser ----> " + address);
+        System.out.println("data ---->" + new String(data,0,length));
+        //释放资源
+        ds.close();
     }
 }

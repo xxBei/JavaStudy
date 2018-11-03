41d4b5cf0276e9c4f451566386e891860cad1785
xxBei <zzw_bei@163.com>
2018-11-03 17:52:22 +0800

    servlet测试

--- Servlet/Example/javaWebDemo/src/com/zzw/serverlet/sayServlet.java
+++ Servlet/Example/javaWebDemo/src/com/zzw/serverlet/sayServlet.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 public class sayServlet implements Servlet {
 
     @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello servlet");
+    }
+
+    @Override
     public void init(ServletConfig servletConfig) throws ServletException {
 
     }
@@ -15,10 +20,6 @@ public class sayServlet implements Servlet {
         return null;
     }
 
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("hello servlet");
-    }
 
     @Override
     public String getServletInfo() {

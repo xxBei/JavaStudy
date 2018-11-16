ac45361b0d62e4ade55e8eaa46d315dda6e17b24
xxBei <zzw_bei@163.com>
2018-11-16 13:52:51 +0800

    设置cookie

--- Cookie&&Session/Example/cookieTest/src/com/zzw/cookie/cookieDemo.java
+++ Cookie&&Session/Example/cookieTest/src/com/zzw/cookie/cookieDemo.java
@@ -2,6 +2,7 @@ package com.zzw.cookie;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,10 +11,26 @@ import java.io.IOException;
 @WebServlet(name = "cookieDemo")
 public class cookieDemo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        解决中文乱码
+        response.setContentType("text/html;charset=utf-8");
+
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie:cookies) {
+                String name = cookie.getName();
+                String value = cookie.getValue();
+            }
+        }
+
+        Cookie cookie = new Cookie("aa","10");
+        response.addCookie(cookie);
+        response.getWriter().write("请求成功...");
+
+
 
     }
 }

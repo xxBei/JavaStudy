f3c67fc26164bb8c05d3b12f6cf1323022059f7d
xxBei <zzw_bei@163.com>
2018-11-23 22:14:28 +0800

    学生管理系统

---
+++ JSP&EL&JSTL/Example/stuManager/src/com/zzw/web/servlet/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.zzw.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns =  "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+
+    }
+}

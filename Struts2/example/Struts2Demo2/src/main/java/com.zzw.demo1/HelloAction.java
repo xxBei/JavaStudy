28f6326d18cc4cce4d3f369db9ba5a4b975e00f7
xxBei <zzw_bei@163.com>
2019-01-13 20:29:27 +0800

    编写Action方法一:自己动手写execute()方法

--- Struts2/example/Struts2Demo2/src/main/java/com.zzw.demo1/HelloAction.java
+++ Struts2/example/Struts2Demo2/src/main/java/com.zzw.demo1/HelloAction.java
@@ -1,5 +1,9 @@
 package com.zzw.demo1;
 
+/**
+ * 编写Action的方式一:
+ *      自己动手写execute()方法
+ * */
 public class HelloAction {
     public String execute(){
         System.out.println("HelloAction被执行了...");

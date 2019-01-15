6b4ee17a78c5e463fa4ec61bb607f51ffe2ccd4b
xxBei <zzw_bei@163.com>
2019-01-15 17:41:59 +0800

    Struts2和Hibernate:客户关系管理系统前期配置

---
+++ Struts2/example/Struts2_crm/src/main/java/com/zzw/Utils/HibernateUtils.java
@@ -0,0 +1,23 @@
+package com.zzw.Utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+    public static Configuration cfg;
+    public static SessionFactory sf;
+
+    static {
+        cfg = new Configuration().configure();
+        sf = cfg.buildSessionFactory();
+    }
+
+    public static Session openSession(){
+        return sf.openSession();
+    }
+
+    public static Session getCurrentSession(){
+        return sf.getCurrentSession();
+    }
+}
---
+++ Struts2/example/Struts2_crm/src/main/webapp/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zBei
+  Date: 2019/1/15
+  Time: 17:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>客户管理系统</title>
+</head>
+<body>
+
+</body>
+</html>

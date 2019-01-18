bcc744c9df5bc260abe09569be51eb1c60124a9a
xxBei <zzw_bei@163.com>
2019-01-18 17:00:47 +0800

    Struts2:ActionContext接收参数

---
+++ Struts2/example/Struts2_demo3/src/main/java/com/zzw/ActionContext/RequestDemo1.java
@@ -0,0 +1,26 @@
+package com.zzw.ActionContext;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.dispatcher.HttpParameters;
+
+public class RequestDemo1 extends ActionSupport {
+    @Override
+    public String execute() throws Exception {
+        /**
+         * 一、接收参数
+         * 利用Struts2中的ActionContext对象
+         * */
+        ActionContext context = ActionContext.getContext();
+        //HttpParameters相当于Map<String,String[]> map = request.getParameters()
+        //context.getParameters()可以用于获取从页面中传递的值
+        HttpParameters parameters = context.getParameters();
+        //将值通过遍历循环出来
+        for (String key : parameters.keySet()) {
+            //
+            System.out.println("key:"+key+"------"+"value:"+parameters.get(key));
+        }
+
+        return NONE;
+    }
+}
---
+++ Struts2/example/Struts2_demo3/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@page contentType="text/html; charset=utf-8" language="java" %>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="requestDemo1.action" method="get">
+        <input name="username" type="text"><br>
+        <input name="password" type="password"><br>
+        <input type="submit" value="登录">
+    </form>
+</body>
+</html>

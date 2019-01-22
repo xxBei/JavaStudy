9c16d1981337cf8abd6354aa601c0b6247312fb9
xxBei <zzw_bei@163.com>
2019-01-22 10:51:43 +0800

    OGNL在Struts下的使用

--- Struts2/example/OGNLDemo1/src/main/webapp/index.jsp
+++ Struts2/example/OGNLDemo1/src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@page language="java" contentType="text/html; charset=UTF-8" %>
+<%@taglib prefix="s" uri="/struts-tags" %>
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,6 +10,10 @@
     <title>Document</title>
 </head>
 <body>
-
+    <h3>OGNL访问对象方法</h3>
+    struts的字符长度为:<s:property value="'struts'.length"/>
+    <h3>OGNL访问静态方法</h3>
+    <%--静态方法默认是关闭,需要在配置文件开启--%>
+    随机数:<s:property value="@java.lang.Math@random()" />
 </body>
 </html>
--- Struts2/example/OGNLDemo1/src/test/java/com/zzw/ognl/OgnlDemo1.java
+++ Struts2/example/OGNLDemo1/src/test/java/com/zzw/ognl/OgnlDemo1.java
@@ -1,5 +1,6 @@
 package com.zzw.ognl;
 
+import com.zzw.domain.User;
 import ognl.Ognl;
 import ognl.OgnlContext;
 import ognl.OgnlException;
@@ -11,9 +12,10 @@ import org.junit.Test;
 public class OgnlDemo1 {
 
     @Test
-    /*
-    * OGNL调用对象的方法
-    * */
+    /**
+     * OGNL调用对象的方法
+     * */
+
     public void test01() throws OgnlException {
         //获取Context
         OgnlContext context = new OgnlContext();
@@ -22,5 +24,49 @@ public class OgnlDemo1 {
         //获取数据或叫执行表达式 --- 查询字符串的长度
         Object obj = Ognl.getValue("'hello OGNL'.length()", context, root);
         System.out.println(obj);
+    }
+
+    @Test
+    /**
+     * OGNL调用静态对象的方法
+     * */
+    public void test02() throws OgnlException {
+        //获取Context
+        OgnlContext context = new OgnlContext();
+        //获取根对象
+        Object root = context.getRoot();
+        //使用表达式,执行静态方法,(@类名@方法)
+        Object value = Ognl.getValue("@java.lang.Math@random()", context, root);
+        System.out.println(value);
+
+    }
+
+    @Test
+    /**
+     * OGNL访问Root中的数据，不需要加 #
+     * */
+    public void test03() throws OgnlException {
+        //获取content
+        OgnlContext context = new OgnlContext();
+        User user = new User("zbei","123");
+        //给root设置值
+        context.setRoot(user);
+        //获取root中的值
+        Object root = context.getRoot();
+        Object username = Ognl.getValue("username", context, root);
+        Object password = Ognl.getValue("password", context, root);
+        System.out.println(username+"=="+password);
+    }
+
+    @Test
+    /**
+     * OGNL访问context中的数据,需要加#
+     * */
+    public void test04() throws OgnlException {
+        OgnlContext context = new OgnlContext();
+        Object root = context.getRoot();
+        context.put("name","abc");
+        Object value = Ognl.getValue("#name", context, root);
+        System.out.println(value);
     }
 }

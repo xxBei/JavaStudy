f25c1fb61bc008b2c475ec16e4df2af05866ac55
xxBei <zzw_bei@163.com>
2019-02-07 23:01:15 +0800

    Spring的AOP入门：权限校验(配置切入点和切入面)

---
+++ Spring/Example/SpringDemo2_AOP/src/main/java/com/zzw/demo1/MyAspectXML.java
@@ -0,0 +1,12 @@
+package com.zzw.demo1;
+
+public class MyAspectXML {
+
+    public void checkPri(){
+        System.out.println("权限校验==============");
+    }
+
+    public void checkLog(){
+        System.out.println("日志校验==============");
+    }
+}
